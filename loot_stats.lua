7377e2aaf01429b7776050c804c12b1d256c79bd
EgzoT <przemek7575@prokonto.pl>
2021-04-12 22:52:22 +0200

    Move rest UI elements to UI object

--- loot_stats.lua
+++ loot_stats.lua
@@ -29,7 +29,7 @@ end
 -------------------------------------------------
 
 local loadedVersionItems = 0
-local actualVisibleTab = {tab = 0, info = 0}
+actualVisibleTab = { tab = 0, info = 0 }
 local ownParser = false
 
 function initLootChecker()
@@ -92,7 +92,7 @@ function terminateLootChecker()
   disconnect(g_game, { onClientVersionChange = loadClientVersionItems })
 end
 
-local lootCheckerTable = {}
+lootCheckerTable = {}
 
 function helpReturnLootCheckerTable()
   return lootCheckerTable
@@ -224,7 +224,7 @@ function checkLootTextMessage(messageMode, message)
   end
 
   if ui.mainWindow:isVisible() then
-		refreshDataInUI()
+		ui:refreshListElements()
 	end
 end
 
@@ -290,47 +290,6 @@ function editMonstersLootOutfit(creature)
       end
     end
   end, 1000)
-end
-
--------------------------------------------------
---Format data------------------------------------
--------------------------------------------------
-
-function formatUINumber(value, numbers, cutDigits)
-  numbers = numbers or 0
-  cutDigits = cutDigits or false
-
-  if value - math.floor(value) == 0 then
-    return value
-  end
-
-  local decimalPart = 0
-  local intPart = 0
-
-  if value > 1 then
-    decimalPart = value - math.floor(value)
-    intPart = math.floor(value)
-  else
-    decimalPart = value
-  end
-
-  local firstNonZeroPos = math.floor(math.log10(decimalPart)) + 1
-
-  local numberOfPoints = 1
-  if cutDigits then
-    numberOfPoints = math.pow(10, numbers - math.floor(math.log10(value)) - 1)
-  else
-    numberOfPoints = math.pow(10, firstNonZeroPos * -1 + numbers)
-  end
-
-  local valuePow = decimalPart * numberOfPoints
-  if valuePow - math.floor(valuePow) >= 0.5 then
-    valuePow = math.ceil(valuePow)
-  else
-    valuePow = math.floor(valuePow)
-  end
-
-  return intPart + valuePow / numberOfPoints
 end
 
 -------------------------------------------------
@@ -401,258 +360,4 @@ function returnAllMonsterCount()
   end
 
   return monsterCount
-end
-
--------------------------------------------------
---Monster view show/hide-------------------------
--------------------------------------------------
-
-function showMonsterView(creature, text)
-  ui.elements.panelCreatureView:setHeight(40)
-  ui.elements.panelCreatureView:setVisible(true)
-
-  local ceatureView = ui.elements.panelCreatureView:getChildById('creatureView')
-  ceatureView:setCreature(creature)
-
-  local creatureText = ui.elements.panelCreatureView:getChildById('textCreatureView')
-  creatureText:setText(text)
-end
-
-function hideMonsterView()
-  ui.elements.panelCreatureView:setHeight(0)
-  ui.elements.panelCreatureView:setVisible(false)
-end
-
--------------------------------------------------
---On click UI------------------------------------
--------------------------------------------------
-
-function changeWhenClickWidget(self, mousePosition, mouseButton)
-  if mouseButton == MouseLeftButton then
-    ui.elements.monstersTab:setOn(false)
-
-    showMonsterView(self:getChildById('creature'):getCreature(), self:getText())
-
-    local monsterName = ''
-    for word in string.gmatch(self:getText(), '([^'..'\n'..']+)') do
-      monsterName = word
-      break
-    end
-
-    if monsterName then
-      refreshLootItems(monsterName)
-    end
-  end
-end
-
-function whenClickMonstersTab(self, mousePosition, mouseButton)
-  if mouseButton == MouseLeftButton then
-    ui.elements.allLootTab:setOn(false)
-    ui.elements.monstersTab:setOn(true)
-    refreshLootMonsters()
-    hideMonsterView()
-  end
-end
-
-function whenClickAllLootTab(self, mousePosition, mouseButton)
-  if mouseButton == MouseLeftButton then
-    ui.elements.monstersTab:setOn(false)
-    ui.elements.allLootTab:setOn(true)
-    refreshLootItems('*all')
-    hideMonsterView()
-  end
-end
-
--------------------------------------------------
---Add to UI--------------------------------------
--------------------------------------------------
-
-local tableWithLootItemsUI = {}
-
-function destroyLootAndMonsterItemsUI()
-  for a,b in pairs(tableWithLootItemsUI) do
-  	b:destroy()
-  	tableWithLootItemsUI[a] = nil
-  end
-end
-
-function refreshLootItems(monsterName)
-  local itemTable = {}
-  if monsterName == '*all' then
-    itemTable = returnAllLoot()
-  else
-    itemTable = returnMonsterLoot(monsterName)
-  end
-
-  actualVisibleTab.tab = 'loot'
-  actualVisibleTab.info = monsterName
-
-  local layout = ui.elements.itemsPanel:getLayout()
-  layout:disableUpdates()
-
-  destroyLootAndMonsterItemsUI()
-  ui.elements.itemsPanel:destroyChildren()
-
-  for a,b in pairs(itemTable) do
-    tableWithLootItemsUI[a] = g_ui.createWidget('LootItemBox', ui.elements.itemsPanel)
-
-    local text = a..'\n'..'Count: '..b.count
-
-    if not b.plural then
-      local chanceToLoot = 0
-      if monsterName ~= '*all' then
-        chanceToLoot = b.count * 100 / returnMonsterCount(monsterName)
-      else
-        chanceToLoot = b.count * 100 / returnAllMonsterCount()
-      end
-      text = text..'\n'..'Chance: '..formatUINumber(chanceToLoot, 3, true)..' %'
-    else
-      local chanceToLoot = 0
-      if monsterName ~= '*all' then
-        if b.count > returnMonsterCount(monsterName) then
-          chanceToLoot = b.count / returnMonsterCount(monsterName)
-          text = text..'\n'..'Average: '..formatUINumber(chanceToLoot, 3, true)..' / 1'
-        else
-          chanceToLoot = b.count * 100 / returnMonsterCount(monsterName)
-          text = text..'\n'..'Chance: '..formatUINumber(chanceToLoot, 3, true)..' %'
-        end
-      else
-        if b.count > returnAllMonsterCount() then
-          chanceToLoot = b.count / returnAllMonsterCount()
-          text = text..'\n'..'Average: '..formatUINumber(chanceToLoot, 3, true)..' / 1'
-        else
-          chanceToLoot = b.count * 100 / returnAllMonsterCount()
-          text = text..'\n'..'Chance: '..formatUINumber(chanceToLoot, 3, true)..' %'
-        end
-      end
-    end
-
-    tableWithLootItemsUI[a]:setText(text)
-
-    local item = nil
-    local findItemByName = g_things.findItemTypeByName(a)
-    if findItemByName:getClientId() ~= 0 then
-      item = Item.create(findItemByName:getClientId())
-    else
-      item = Item.create(3547)
-    end
-
-    if b.plural then
-      if b.count > 100 then
-        item:setCount(100)
-      else
-        item:setCount(b.count)
-      end
-    end
-
-    local itemWidget = tableWithLootItemsUI[a]:getChildById('item')
-    itemWidget:setItem(item)
-  end
-
-  layout:enableUpdates()
-  layout:update()
-end
-
-function refreshLootMonsters()
-  local layout = ui.elements.itemsPanel:getLayout()
-  layout:disableUpdates()
-
-  destroyLootAndMonsterItemsUI()
-  ui.elements.itemsPanel:destroyChildren()
-
-  actualVisibleTab.tab = 'monster'
-  actualVisibleTab.info = 0
-
-  for a,b in pairs(returnAllMonsters()) do
-    tableWithLootItemsUI[a] = g_ui.createWidget('LootMonsterBox', ui.elements.itemsPanel)
-
-    local text = a..'\n'..'Count: '..b.count
-
-    local chanceMonster = b.count * 100 / returnAllMonsterCount()
-    text = text..'\n'..'Chance: '..formatUINumber(chanceMonster, 3, true)..' %'
-
-    tableWithLootItemsUI[a]:setText(text)
-
-    local uiCreature = Creature.create()
-    uiCreature:setDirection(2)
-
-    if b.outfit then
-      uiCreature:setOutfit(b.outfit)
-    else
-      local noOutfit = {type = 160, feet = 114, addons = 0, legs = 114, auxType = 7399, head = 114, body = 114}
-      uiCreature:setOutfit(noOutfit)
-    end
-
-    local itemWidget = tableWithLootItemsUI[a]:getChildById('creature')
-    itemWidget:setCreature(uiCreature)
-
-    --On click action
-    tableWithLootItemsUI[a].onMouseRelease = changeWhenClickWidget
-  end
-
-  layout:enableUpdates()
-  layout:update()
-end
-
-function clearAllUIAndTable()
-  local yesCallback = function()
-    local layout = ui.elements.itemsPanel:getLayout()
-    layout:disableUpdates()
-
-    destroyLootAndMonsterItemsUI()
-    ui.elements.itemsPanel:destroyChildren()
-
-    ui.elements.allLootTab:setOn(false)
-    ui.elements.monstersTab:setOn(false)
-    hideMonsterView()
-    lootCheckerTable = {}
-
-    layout:enableUpdates()
-    layout:update()
-
-    saveOverWindow:destroy()
-    saveOverWindow=nil
-  end
-
-  local noCallback = function()
-    saveOverWindow:destroy()
-    saveOverWindow=nil
-  end
-
-  if not saveOverWindow then
-    saveOverWindow = displayGeneralBox(tr('Clear all values'), tr('Do you want clear all values?\nYou will lost all loot data!'), {
-      { text=tr('Yes'), callback = yesCallback},
-      { text=tr('No'), callback = noCallback},
-    anchor=AnchorHorizontalCenter}, yesCallback, noCallback)
-  end
-end
-
-function refreshDataInUI()
-	if actualVisibleTab.tab == 'loot' then
-  	refreshLootItems(actualVisibleTab.info)
-  	if actualVisibleTab.info ~= '*all' then
-  		local creatureText = ui.elements.panelCreatureView:getChildById('textCreatureView')
-
-  		local monster = returnAllMonsters()[actualVisibleTab.info]
-  		local text = actualVisibleTab.info..'\n'..'Count: '..monster.count
-
-    	local chanceMonster = monster.count * 100 / returnAllMonsterCount()
-    	text = text..'\n'..'Chance: '..formatUINumber(chanceMonster, 3, true)..' %'
-    	creatureText:setText(text)
-  	end
-  elseif actualVisibleTab.tab == 'monster' then
-  	refreshLootMonsters()
-  end
-end
-
--------------------------------------------------
---Show loot on screen----------------------------
--------------------------------------------------
-
-function saveCheckboxIconsOnScreen()
-  if ui.elements.showLootOnScreen:isChecked() then
-    g_settings.set('loot_stats_addIconsToScreen', false)
-  else
-    g_settings.set('loot_stats_addIconsToScreen', true)
-  end
 end
--- ui/ui.lua
+++ ui/ui.lua
@@ -5,6 +5,8 @@ function UI()
 
         elements = {};
 
+        listElements = {};
+
         init = function(self)
             g_ui.loadUI("loot_icons")
 
@@ -27,6 +29,9 @@ function UI()
         end;
 
         clear = function(self)
+            -- Destroy created elements
+            self:destroyListElements()
+
             self.moduleButton = nil
             self.mainWindow = nil
 
@@ -41,7 +46,7 @@ function UI()
                 self.mainWindow:setVisible(true)
                 self.moduleButton:setOn(true)
 
-                refreshDataInUI()
+                self:refreshListElements()
             end
         end;
 
@@ -62,8 +67,291 @@ function UI()
         end;
 
         setOnChangeElements = function(self)
-            self.elements.monstersTab.onMouseRelease = whenClickMonstersTab
-            self.elements.allLootTab.onMouseRelease = whenClickAllLootTab
+            self.elements.monstersTab.onMouseRelease = function(widget, mousePosition, mouseButton) self:whenClickMonstersTab(widget, mousePosition, mouseButton) end
+            self.elements.allLootTab.onMouseRelease = function(widget, mousePosition, mouseButton) self:whenClickAllLootTab(widget, mousePosition, mouseButton) end
+        end;
+
+        -- Format data
+
+        formatNumber = function(self, value, numbers, cutDigits)
+            numbers = numbers or 0
+            cutDigits = cutDigits or false
+
+            if value - math.floor(value) == 0 then
+                return value
+            end
+
+            local decimalPart = 0
+            local intPart = 0
+
+            if value > 1 then
+                decimalPart = value - math.floor(value)
+                intPart = math.floor(value)
+            else
+                decimalPart = value
+            end
+
+            local firstNonZeroPos = math.floor(math.log10(decimalPart)) + 1
+
+            local numberOfPoints = 1
+            if cutDigits then
+                numberOfPoints = math.pow(10, numbers - math.floor(math.log10(value)) - 1)
+            else
+                numberOfPoints = math.pow(10, firstNonZeroPos * -1 + numbers)
+            end
+
+            local valuePow = decimalPart * numberOfPoints
+            if valuePow - math.floor(valuePow) >= 0.5 then
+                valuePow = math.ceil(valuePow)
+            else
+                valuePow = math.floor(valuePow)
+            end
+
+            return intPart + valuePow / numberOfPoints
+        end;
+
+        -- Monster view show/hide
+
+        showMonsterView = function(self, creature, text)
+            self.elements.panelCreatureView:setHeight(40)
+            self.elements.panelCreatureView:setVisible(true)
+
+            local ceatureView = self.elements.panelCreatureView:getChildById('creatureView')
+            ceatureView:setCreature(creature)
+
+            local creatureText = self.elements.panelCreatureView:getChildById('textCreatureView')
+            creatureText:setText(text)
+        end;
+
+        hideMonsterView = function(self)
+            self.elements.panelCreatureView:setHeight(0)
+            self.elements.panelCreatureView:setVisible(false)
+        end;
+
+        -- On click actions
+
+        changeWhenClickWidget = function(self, widget, mousePosition, mouseButton)
+            if mouseButton == MouseLeftButton then
+                self.elements.monstersTab:setOn(false)
+
+                self:showMonsterView(widget:getChildById('creature'):getCreature(), widget:getText())
+
+                local monsterName = ''
+                for word in string.gmatch(widget:getText(), '([^'..'\n'..']+)') do
+                    monsterName = word
+                    break
+                end
+
+                if monsterName then
+                    self:refreshLootItems(monsterName)
+                end
+            end
+        end;
+
+        whenClickMonstersTab = function(self, widget, mousePosition, mouseButton)
+            if mouseButton == MouseLeftButton then
+                self.elements.allLootTab:setOn(false)
+                self.elements.monstersTab:setOn(true)
+                self:refreshLootMonsters()
+                self:hideMonsterView()
+            end
+        end;
+
+        whenClickAllLootTab = function(self, widget, mousePosition, mouseButton)
+            if mouseButton == MouseLeftButton then
+                self.elements.monstersTab:setOn(false)
+                self.elements.allLootTab:setOn(true)
+                self:refreshLootItems('*all')
+                self:hideMonsterView()
+            end
+        end;
+
+        -- Add to UI
+
+        destroyListElements = function(self)
+            for a,b in pairs(self.listElements) do
+                b:destroy()
+                self.listElements[a] = nil
+            end
+        end;
+
+        refreshLootItems = function(self, monsterName)
+            local itemTable = {}
+            if monsterName == '*all' then
+                itemTable = returnAllLoot()
+            else
+                itemTable = returnMonsterLoot(monsterName)
+            end
+
+            actualVisibleTab.tab = 'loot'
+            actualVisibleTab.info = monsterName
+
+            local layout = self.elements.itemsPanel:getLayout()
+            layout:disableUpdates()
+
+            self:destroyListElements()
+            self.elements.itemsPanel:destroyChildren()
+
+            for a,b in pairs(itemTable) do
+                self.listElements[a] = g_ui.createWidget('LootItemBox', self.elements.itemsPanel)
+
+                local text = a .. '\n' .. 'Count: ' .. b.count
+
+                if not b.plural then
+                    local chanceToLoot = 0
+                    if monsterName ~= '*all' then
+                        chanceToLoot = b.count * 100 / returnMonsterCount(monsterName)
+                    else
+                        chanceToLoot = b.count * 100 / returnAllMonsterCount()
+                    end
+                    text = text .. '\n' .. 'Chance: ' .. self:formatNumber(chanceToLoot, 3, true) .. ' %'
+                else
+                    local chanceToLoot = 0
+                    if monsterName ~= '*all' then
+                        if b.count > returnMonsterCount(monsterName) then
+                            chanceToLoot = b.count / returnMonsterCount(monsterName)
+                            text = text .. '\n' .. 'Average: ' .. self:formatNumber(chanceToLoot, 3, true) .. ' / 1'
+                        else
+                            chanceToLoot = b.count * 100 / returnMonsterCount(monsterName)
+                            text = text .. '\n' .. 'Chance: ' .. self:formatNumber(chanceToLoot, 3, true) .. ' %'
+                        end
+                    else
+                        if b.count > returnAllMonsterCount() then
+                            chanceToLoot = b.count / returnAllMonsterCount()
+                            text = text .. '\n' .. 'Average: ' .. self:formatNumber(chanceToLoot, 3, true) .. ' / 1'
+                        else
+                            chanceToLoot = b.count * 100 / returnAllMonsterCount()
+                            text = text .. '\n' .. 'Chance: ' .. self:formatNumber(chanceToLoot, 3, true) .. ' %'
+                        end
+                    end
+                end
+
+                self.listElements[a]:setText(text)
+
+                local item = nil
+                local findItemByName = g_things.findItemTypeByName(a)
+                if findItemByName:getClientId() ~= 0 then
+                    item = Item.create(findItemByName:getClientId())
+                else
+                    item = Item.create(3547)
+                end
+
+                if b.plural then
+                    if b.count > 100 then
+                        item:setCount(100)
+                    else
+                        item:setCount(b.count)
+                    end
+                end
+
+                local itemWidget = self.listElements[a]:getChildById('item')
+                itemWidget:setItem(item)
+            end
+
+            layout:enableUpdates()
+            layout:update()
+        end;
+
+        refreshLootMonsters = function(self)
+            local layout = self.elements.itemsPanel:getLayout()
+            layout:disableUpdates()
+
+            self:destroyListElements()
+            self.elements.itemsPanel:destroyChildren()
+
+            actualVisibleTab.tab = 'monster'
+            actualVisibleTab.info = 0
+
+            for a,b in pairs(returnAllMonsters()) do
+                self.listElements[a] = g_ui.createWidget('LootMonsterBox', self.elements.itemsPanel)
+
+                local text = a .. '\n' .. 'Count: ' .. b.count
+
+                local chanceMonster = b.count * 100 / returnAllMonsterCount()
+                text = text .. '\n' .. 'Chance: ' .. self:formatNumber(chanceMonster, 3, true) .. ' %'
+
+                self.listElements[a]:setText(text)
+
+                local uiCreature = Creature.create()
+                uiCreature:setDirection(2)
+
+                if b.outfit then
+                    uiCreature:setOutfit(b.outfit)
+                else
+                    local noOutfit = { type = 160, feet = 114, addons = 0, legs = 114, auxType = 7399, head = 114, body = 114 }
+                    uiCreature:setOutfit(noOutfit)
+                end
+
+                local itemWidget = self.listElements[a]:getChildById('creature')
+                itemWidget:setCreature(uiCreature)
+
+                -- On click action
+                self.listElements[a].onMouseRelease = function(widget, mousePosition, mouseButton) self:changeWhenClickWidget(widget, mousePosition, mouseButton) end
+            end
+
+            layout:enableUpdates()
+            layout:update()
+        end;
+
+        clearData = function(self)
+            local yesCallback = function()
+                local layout = self.elements.itemsPanel:getLayout()
+                layout:disableUpdates()
+
+                self:destroyListElements()
+                self.elements.itemsPanel:destroyChildren()
+
+                self.elements.allLootTab:setOn(false)
+                self.elements.monstersTab:setOn(false)
+                self:hideMonsterView()
+                lootCheckerTable = {}
+
+                layout:enableUpdates()
+                layout:update()
+
+                saveOverWindow:destroy()
+                saveOverWindow = nil
+            end
+
+            local noCallback = function()
+                saveOverWindow:destroy()
+                saveOverWindow = nil
+            end
+
+            if not saveOverWindow then
+                saveOverWindow = displayGeneralBox(tr('Clear all values'), tr('Do you want clear all values?\nYou will lost all loot data!'), {
+                    { text=tr('Yes'), callback = yesCallback },
+                    { text=tr('No'), callback = noCallback },
+                anchor=AnchorHorizontalCenter }, yesCallback, noCallback)
+            end
+        end;
+
+        refreshListElements = function(self)
+            if actualVisibleTab.tab == 'loot' then
+                self:refreshLootItems(actualVisibleTab.info)
+                if actualVisibleTab.info ~= '*all' then
+                    local creatureText = self.elements.panelCreatureView:getChildById('textCreatureView')
+
+                    local monster = returnAllMonsters()[actualVisibleTab.info]
+                    local text = actualVisibleTab.info .. '\n' .. 'Count: ' .. monster.count
+
+                    local chanceMonster = monster.count * 100 / returnAllMonsterCount()
+                    text = text .. '\n' .. 'Chance: ' .. self:formatNumber(chanceMonster, 3, true) .. ' %'
+                    creatureText:setText(text)
+                end
+            elseif actualVisibleTab.tab == 'monster' then
+                self:refreshLootMonsters()
+            end
+        end;
+
+        -- Options
+
+        saveCheckboxIconsOnScreen = function(self)
+            if self.elements.showLootOnScreen:isChecked() then
+                g_settings.set('loot_stats_addIconsToScreen', false)
+            else
+                g_settings.set('loot_stats_addIconsToScreen', true)
+            end
         end;
     }
 
