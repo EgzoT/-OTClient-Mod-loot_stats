61ec93890d857ac73aa7c73bd96d1e53d82e5299
EgzoT <przemek7575@prokonto.pl>
2021-04-13 21:04:52 +0200

    Move create stats functionality to separate file

--- loot_stats.lua
+++ loot_stats.lua
@@ -1,24 +1,26 @@
 -- Imports
 dofile('ui/ui')
+dofile('systems/createStats')
 dofile('ui/showLootOnScreen')
 dofile('systems/itemsXML')
 
 -- Modules
 ui = UI()
+createStats = CreateStats()
 showLootOnScreen = ShowLootOnScreen()
 
 function init()
   ui:init()
+  createStats:init()
 
-  --init all systems
-  initLootChecker()
+  initEditMonstersLootOutfit()
 end
 
 function terminate()
   ui:terminate()
+  createStats:terminate()
 
-  --terminate all systems
-  terminateLootChecker()
+  terminateEditMonstersLootOutfit()
 
   -- Destroy created UI items on screen
   showLootOnScreen:destroy()
@@ -28,69 +30,7 @@ end
 --Scripts----------------------------------------
 -------------------------------------------------
 
-local loadedVersionItems = 0
 actualVisibleTab = { tab = 0, info = 0 }
-local ownParser = false
-
-function initLootChecker()
-  connect(g_game, {onTextMessage = checkLootTextMessage})
-  initEditMonstersLootOutfit()
-
-  connect(g_game, { onClientVersionChange = loadClientVersionItems })
-
-  if (loadedVersionItems == 0 and g_game.getClientVersion() ~= 0) or (g_game.getClientVersion() ~= 0 and loadedVersionItems ~= g_game.getClientVersion()) then
-  	loadClientVersionItems()
-  end
-
-  --Open monster tab as default
-  actualVisibleTab.tab = 'monster'
-  ui.elements.monstersTab:setOn(true)
-end
-
-function loadClientVersionItems()
-	local version = g_game.getClientVersion()
-
-	if version ~= loadedVersionItems then
-		if not g_resources.directoryExists('items_versions/'..version) then
-			pwarning("Directory: items_versions/"..version.."/ doesn't exist!")
-			pwarning("Add "..version.." directory to items_versions/ with correct version items.otb and items.xml!")
-			loadedVersionItems = 0
-			g_modules.getModule('loot_stats'):unload()
-			modules.client_modulemanager.refreshLoadedModules()
-			return
-		end
-		if not g_resources.fileExists('items_versions/'..version..'/items.otb') then
-			pwarning("File: items_versions/"..version.."/ doesn't exist!")
-			pwarning("Add correct version items.otb to items_versions/"..version.."/!")
-			loadedVersionItems = 0
-			g_modules.getModule('loot_stats'):unload()
-			modules.client_modulemanager.refreshLoadedModules()
-			return
-		end
-		if not g_resources.fileExists('items_versions/'..version..'/items.xml') then
-			pwarning("File: items_versions/"..version.." doesn't exist!")
-			pwarning("Add correct version items.xml to items_versions/"..version.."/!")
-			loadedVersionItems = 0
-			g_modules.getModule('loot_stats'):unload()
-			modules.client_modulemanager.refreshLoadedModules()
-			return
-		end
-
-
-		g_things.loadOtb('items_versions/'..version..'/items.otb')
-		g_things.loadXml('items_versions/'..version..'/items.xml')
-    checkParserType()
-
-		loadedVersionItems = version
-	end
-end
-
-function terminateLootChecker()
-  disconnect(g_game, {onTextMessage = checkLootTextMessage})
-  terminateEditMonstersLootOutfit()
-
-  disconnect(g_game, { onClientVersionChange = loadClientVersionItems })
-end
 
 lootCheckerTable = {}
 
@@ -98,160 +38,8 @@ function helpReturnLootCheckerTable()
   return lootCheckerTable
 end
 
-function checkLootTextMessage(messageMode, message)
-	if loadedVersionItems == 0 then
-		return
-	end
-
-  local fromLootValue, toLootValue = string.find(message, 'Loot of ')
-  if toLootValue then
-    --Return Monster
-    local lootMonsterName = string.sub(message, toLootValue + 1, string.find(message, ':') - 1)
-    local isAFromLootValue, isAToLootValue = string.find(lootMonsterName, 'a ')
-    if isAToLootValue then
-      lootMonsterName = string.sub(lootMonsterName, isAToLootValue + 1, string.len(lootMonsterName))
-    end
-    local isANFromLootValue, isANToLootValue = string.find(lootMonsterName, 'an ')
-    if isANToLootValue then
-      lootMonsterName = string.sub(lootMonsterName, isANToLootValue + 1, string.len(lootMonsterName))
-    end
-    --If no monster then add Monster to table
-    if not lootCheckerTable[lootMonsterName] then
-      lootCheckerTable[lootMonsterName] = {loot = {}, count = 0}
-    end
-    --Update monster kill count information
-    lootCheckerTable[lootMonsterName].count = lootCheckerTable[lootMonsterName].count + 1
-
-    --Return Loot
-    local lootString = string.sub(message, string.find(message, ': ') + 2, string.len(message))
-    --If dot at the ned of sentence (OTS only), delete it.
-    if string.sub(lootString, string.len(lootString)) == '.' then
-      lootString = string.sub(lootString, 0, string.len(lootString) - 1)
-    end
-
-    local lootToScreen = {}
-    for word in string.gmatch(lootString, '([^,]+)') do
-      --delete first space
-      if string.sub(word, 0, 1) == ' ' then
-        word = string.sub(word, 2, string.len(word))
-      end
-
-      --delete 'a ' / 'an '
-      local isAToLootValue, isAFromLootValue = string.find(word, 'a ')
-      if isAFromLootValue then
-        word = string.sub(word, isAFromLootValue + 1, string.len(word))
-      end
-      local isANToLootValue, isANFromLootValue = string.find(word, 'an ')
-      if isANFromLootValue then
-        word = string.sub(word, isANFromLootValue + 1, string.len(word))
-      end
-
-      --Check is first sign is number
-      if type(tonumber(string.sub(word, 0, 1))) == 'number' then
-        local itemCount = tonumber(string.match(word, "%d+"))
-        local delFN, delLN = string.find(word, itemCount)
-        local itemWord = string.sub(word, delLN + 2)
-
-        function returnPluralNameFromLoot()
-          for a,b in pairs(lootCheckerTable[lootMonsterName].loot) do
-            if b.plural == itemWord then
-              return a
-            end
-          end
-
-          return false
-        end
-
-        local isPluralNameInLoot = returnPluralNameFromLoot()
-        if isPluralNameInLoot then
-          if not lootCheckerTable[lootMonsterName].loot[isPluralNameInLoot] then
-            lootCheckerTable[lootMonsterName].loot[isPluralNameInLoot] = {}
-            lootCheckerTable[lootMonsterName].loot[isPluralNameInLoot].count = 0
-          end
-          if not lootToScreen[isPluralNameInLoot] then
-          	lootToScreen[isPluralNameInLoot] = {}
-            lootToScreen[isPluralNameInLoot].count = 0
-          end
-          lootCheckerTable[lootMonsterName].loot[isPluralNameInLoot].count = lootCheckerTable[lootMonsterName].loot[isPluralNameInLoot].count + itemCount
-          lootToScreen[isPluralNameInLoot].count = lootToScreen[isPluralNameInLoot].count + itemCount
-        else
-          local pluralNameToSingular = convertPluralToSingular(itemWord)
-          if pluralNameToSingular then
-            if not lootCheckerTable[lootMonsterName].loot[pluralNameToSingular] then
-              lootCheckerTable[lootMonsterName].loot[pluralNameToSingular] = {}
-              lootCheckerTable[lootMonsterName].loot[pluralNameToSingular].count = 0
-            end
-            if not lootCheckerTable[lootMonsterName].loot[pluralNameToSingular].plural then
-              lootCheckerTable[lootMonsterName].loot[pluralNameToSingular].plural = itemWord
-            end
-            if not lootToScreen[pluralNameToSingular] then
-            	lootToScreen[pluralNameToSingular] = {}
-              lootToScreen[pluralNameToSingular].count = 0
-            end
-            lootCheckerTable[lootMonsterName].loot[pluralNameToSingular].count = lootCheckerTable[lootMonsterName].loot[pluralNameToSingular].count + itemCount
-          	lootToScreen[pluralNameToSingular].count = lootToScreen[pluralNameToSingular].count + itemCount
-          else
-            if not lootCheckerTable[lootMonsterName].loot[word] then
-              lootCheckerTable[lootMonsterName].loot[word] = {}
-              lootCheckerTable[lootMonsterName].loot[word].count = 0
-            end
-            if not lootToScreen[word] then
-            	lootToScreen[word] = {}
-              lootToScreen[word].count = 0
-            end
-            lootCheckerTable[lootMonsterName].loot[word].count = lootCheckerTable[lootMonsterName].loot[word].count + 1
-          	lootToScreen[word].count = lootToScreen[word].count + 1
-          end
-        end
-      else
-        if not lootCheckerTable[lootMonsterName].loot[word] then
-          lootCheckerTable[lootMonsterName].loot[word] = {}
-          lootCheckerTable[lootMonsterName].loot[word].count = 0
-        end
-        if not lootToScreen[word] then
-        	lootToScreen[word] = {}
-          lootToScreen[word].count = 0
-        end
-        lootCheckerTable[lootMonsterName].loot[word].count = lootCheckerTable[lootMonsterName].loot[word].count + 1
-      	lootToScreen[word].count = lootToScreen[word].count + 1
-      end
-    end
-
-    if ui.elements.showLootOnScreen:isChecked() then
-      showLootOnScreen:add(lootToScreen)
-    end
-    lootToScreen = {}
-  end
-
-  if ui.mainWindow:isVisible() then
-		ui:refreshListElements()
-	end
-end
-
-function checkParserType()
-  if g_things.findItemTypeByPluralName then
-    ownParser = false
-  else
-    ownParser = ItemsXML()
-    ownParser:parseItemsXML(g_resources.readFileContents('items_versions/' .. g_game.getClientVersion() .. '/items.xml'))
-  end
-end
-
-function convertPluralToSingular(searchWord)
-  if not ownParser then
-    local item = g_things.findItemTypeByPluralName(searchWord)
-    if not item:isNull() then
-      return item:getName()
-    else
-      return false
-    end
-  else
-    return ownParser:convertPluralToSingular(searchWord)
-  end
-end
-
 function getParserType()
-  if ownParser then
+  if createStats.ownParser then
     return "Own XML parser"
   else
     return "OTClient inbuild XML parser"
@@ -360,4 +148,26 @@ function returnAllMonsterCount()
   end
 
   return monsterCount
+end
+
+-- File system
+
+function directoryExists(path)
+  return g_resources.directoryExists(path)
+end
+
+function fileExists(path)
+  return g_resources.fileExists(path)
+end
+
+function readFileContents(path)
+  return g_resources.readFileContents(path)
+end
+
+function loadOtb(path)
+  return g_things.loadOtb(path)
+end
+
+function loadXml(path)
+  return g_things.loadXml(path)
 end
---
+++ systems/createStats.lua
@@ -0,0 +1,242 @@
+function CreateStats()
+    local createStats = {
+        loadedVersionItems = 0;
+        ownParser = false;
+
+        _loadClientVersionItems = nil;
+        _checkLootTextMessage = nil;
+
+        init = function(self)
+            self._loadClientVersionItems = function() self:loadClientVersionItems() end
+            self._checkLootTextMessage = function(messageMode, message) self:checkLootTextMessage(messageMode, message) end
+
+            connect(g_game, { onClientVersionChange = self._loadClientVersionItems })
+            connect(g_game, { onTextMessage = self._checkLootTextMessage })
+
+            if (self.loadedVersionItems == 0 and g_game.getClientVersion() ~= 0) or (g_game.getClientVersion() ~= 0 and self.loadedVersionItems ~= g_game.getClientVersion()) then
+                self:loadClientVersionItems()
+            end
+
+            -- Open monster tab as default
+            actualVisibleTab.tab = 'monster'
+            ui.elements.monstersTab:setOn(true)
+        end;
+
+        terminate = function(self)
+            disconnect(g_game, { onClientVersionChange = self._loadClientVersionItems })
+            disconnect(g_game, { onTextMessage = self._checkLootTextMessage })
+        end;
+
+        -- Load items
+
+        loadClientVersionItems = function(self)
+            local version = g_game.getClientVersion()
+
+            if version ~= self.loadedVersionItems then
+                if not directoryExists('items_versions/' .. version) then
+                    pwarning("Directory: items_versions/" .. version .. "/ doesn't exist!")
+                    pwarning("Add " .. version .. " directory to items_versions/ with correct version items.otb and items.xml!")
+                    self.loadedVersionItems = 0
+                    g_modules.getModule('loot_stats'):unload()
+                    modules.client_modulemanager.refreshLoadedModules()
+                    return
+                end
+
+                if not fileExists('items_versions/' .. version .. '/items.otb') then
+                    pwarning("File: items_versions/" .. version .. "/ doesn't exist!")
+                    pwarning("Add correct version items.otb to items_versions/" .. version .. "/!")
+                    self.loadedVersionItems = 0
+                    g_modules.getModule('loot_stats'):unload()
+                    modules.client_modulemanager.refreshLoadedModules()
+                    return
+                end
+
+                if not fileExists('items_versions/' .. version .. '/items.xml') then
+                    pwarning("File: items_versions/" .. version .. " doesn't exist!")
+                    pwarning("Add correct version items.xml to items_versions/" .. version .. "/!")
+                    self.loadedVersionItems = 0
+                    g_modules.getModule('loot_stats'):unload()
+                    modules.client_modulemanager.refreshLoadedModules()
+                    return
+                end
+
+                loadOtb('items_versions/' .. version .. '/items.otb')
+                loadXml('items_versions/' .. version .. '/items.xml')
+                self:checkParserType()
+
+                self.loadedVersionItems = version
+            end
+        end;
+
+        checkParserType = function(self)
+            if g_things.findItemTypeByPluralName then
+                self.ownParser = false
+            else
+                self.ownParser = ItemsXML()
+                self.ownParser:parseItemsXML(readFileContents('items_versions/' .. g_game.getClientVersion() .. '/items.xml'))
+            end
+        end;
+
+        -- Convert plural to singular
+
+        convertPluralToSingular = function(self, searchWord)
+            if not self.ownParser then
+                local item = g_things.findItemTypeByPluralName(searchWord)
+                if not item:isNull() then
+                    return item:getName()
+                else
+                    return false
+                end
+            else
+                return self.ownParser:convertPluralToSingular(searchWord)
+            end
+        end;
+
+        -- Parse message logs
+
+        returnPluralNameFromLoot = function(lootMonsterName, itemWord)
+            for a,b in pairs(lootCheckerTable[lootMonsterName].loot) do
+                if b.plural == itemWord then
+                    return a
+                end
+            end
+
+            return false
+        end;
+
+        checkLootTextMessage = function(self, messageMode, message)
+            if self.loadedVersionItems == 0 then
+                return
+            end
+
+            local fromLootValue, toLootValue = string.find(message, 'Loot of ')
+            if toLootValue then
+                -- Return monster
+                local lootMonsterName = string.sub(message, toLootValue + 1, string.find(message, ':') - 1)
+                local isAFromLootValue, isAToLootValue = string.find(lootMonsterName, 'a ')
+                if isAToLootValue then
+                    lootMonsterName = string.sub(lootMonsterName, isAToLootValue + 1, string.len(lootMonsterName))
+                end
+
+                local isANFromLootValue, isANToLootValue = string.find(lootMonsterName, 'an ')
+                if isANToLootValue then
+                    lootMonsterName = string.sub(lootMonsterName, isANToLootValue + 1, string.len(lootMonsterName))
+                end
+
+                -- If no monster then add monster to table
+                if not lootCheckerTable[lootMonsterName] then
+                    lootCheckerTable[lootMonsterName] = { loot = {}, count = 0 }
+                end
+
+                -- Update monster kill count information
+                lootCheckerTable[lootMonsterName].count = lootCheckerTable[lootMonsterName].count + 1
+
+                -- Return Loot
+                local lootString = string.sub(message, string.find(message, ': ') + 2, string.len(message))
+                -- If dot at the ned of sentence (OTS only), delete it
+                if string.sub(lootString, string.len(lootString)) == '.' then
+                    lootString = string.sub(lootString, 0, string.len(lootString) - 1)
+                end
+
+                local lootToScreen = {}
+                for word in string.gmatch(lootString, '([^,]+)') do
+                    -- Delete first space
+                    if string.sub(word, 0, 1) == ' ' then
+                        word = string.sub(word, 2, string.len(word))
+                    end
+
+                    -- Delete 'a ' / 'an '
+                    local isAToLootValue, isAFromLootValue = string.find(word, 'a ')
+                    if isAFromLootValue then
+                        word = string.sub(word, isAFromLootValue + 1, string.len(word))
+                    end
+
+                    local isANToLootValue, isANFromLootValue = string.find(word, 'an ')
+                    if isANFromLootValue then
+                        word = string.sub(word, isANFromLootValue + 1, string.len(word))
+                    end
+
+                    -- Check is first sign is number
+                    if type(tonumber(string.sub(word, 0, 1))) == 'number' then
+                        local itemCount = tonumber(string.match(word, "%d+"))
+                        local delFN, delLN = string.find(word, itemCount)
+                        local itemWord = string.sub(word, delLN + 2)
+                        local isPluralNameInLoot = self.returnPluralNameFromLoot(lootMonsterName, itemWord)
+
+                        if isPluralNameInLoot then
+                            if not lootCheckerTable[lootMonsterName].loot[isPluralNameInLoot] then
+                                lootCheckerTable[lootMonsterName].loot[isPluralNameInLoot] = {}
+                                lootCheckerTable[lootMonsterName].loot[isPluralNameInLoot].count = 0
+                            end
+
+                            if not lootToScreen[isPluralNameInLoot] then
+                                lootToScreen[isPluralNameInLoot] = {}
+                                lootToScreen[isPluralNameInLoot].count = 0
+                            end
+
+                            lootCheckerTable[lootMonsterName].loot[isPluralNameInLoot].count = lootCheckerTable[lootMonsterName].loot[isPluralNameInLoot].count + itemCount
+                            lootToScreen[isPluralNameInLoot].count = lootToScreen[isPluralNameInLoot].count + itemCount
+                        else
+                            local pluralNameToSingular = self:convertPluralToSingular(itemWord)
+                            if pluralNameToSingular then
+                                if not lootCheckerTable[lootMonsterName].loot[pluralNameToSingular] then
+                                    lootCheckerTable[lootMonsterName].loot[pluralNameToSingular] = {}
+                                    lootCheckerTable[lootMonsterName].loot[pluralNameToSingular].count = 0
+                                end
+
+                                if not lootCheckerTable[lootMonsterName].loot[pluralNameToSingular].plural then
+                                    lootCheckerTable[lootMonsterName].loot[pluralNameToSingular].plural = itemWord
+                                end
+
+                                if not lootToScreen[pluralNameToSingular] then
+                                    lootToScreen[pluralNameToSingular] = {}
+                                    lootToScreen[pluralNameToSingular].count = 0
+                                end
+
+                                lootCheckerTable[lootMonsterName].loot[pluralNameToSingular].count = lootCheckerTable[lootMonsterName].loot[pluralNameToSingular].count + itemCount
+                                lootToScreen[pluralNameToSingular].count = lootToScreen[pluralNameToSingular].count + itemCount
+                            else
+                                if not lootCheckerTable[lootMonsterName].loot[word] then
+                                    lootCheckerTable[lootMonsterName].loot[word] = {}
+                                    lootCheckerTable[lootMonsterName].loot[word].count = 0
+                                end
+
+                                if not lootToScreen[word] then
+                                    lootToScreen[word] = {}
+                                    lootToScreen[word].count = 0
+                                end
+
+                                lootCheckerTable[lootMonsterName].loot[word].count = lootCheckerTable[lootMonsterName].loot[word].count + 1
+                                lootToScreen[word].count = lootToScreen[word].count + 1
+                            end
+                        end
+                    else
+                        if not lootCheckerTable[lootMonsterName].loot[word] then
+                            lootCheckerTable[lootMonsterName].loot[word] = {}
+                            lootCheckerTable[lootMonsterName].loot[word].count = 0
+                        end
+
+                        if not lootToScreen[word] then
+                            lootToScreen[word] = {}
+                            lootToScreen[word].count = 0
+                        end
+
+                        lootCheckerTable[lootMonsterName].loot[word].count = lootCheckerTable[lootMonsterName].loot[word].count + 1
+                        lootToScreen[word].count = lootToScreen[word].count + 1
+                    end
+                end
+
+                if ui.elements.showLootOnScreen:isChecked() then
+                    showLootOnScreen:add(lootToScreen)
+                end
+                lootToScreen = {}
+            end
+
+            if ui.mainWindow:isVisible() then
+                ui:refreshListElements()
+            end
+        end;
+    }
+
+    return createStats
+end
