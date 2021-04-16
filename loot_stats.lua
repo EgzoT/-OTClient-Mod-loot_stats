22eb471ce99195566d77ff0113f5f25d178cb5c9
EgzoT <przemek7575@prokonto.pl>
2021-04-16 23:18:00 +0200

    Adjust indentations in loot_stats.lua to project

--- loot_stats.lua
+++ loot_stats.lua
@@ -10,45 +10,45 @@ createStats = CreateStats()
 ui = UI()
 
 function init()
-  store:init()
-  createStats:init()
-  ui:init()
+    store:init()
+    createStats:init()
+    ui:init()
 end
 
 function terminate()
-  ui:terminate()
-  createStats:terminate()
-  store:terminate()
+    ui:terminate()
+    createStats:terminate()
+    store:terminate()
 end
 
 -- File system
 
 function directoryExists(path)
-  return g_resources.directoryExists(path)
+    return g_resources.directoryExists(path)
 end
 
 function fileExists(path)
-  return g_resources.fileExists(path)
+    return g_resources.fileExists(path)
 end
 
 function readFileContents(path)
-  return g_resources.readFileContents(path)
+    return g_resources.readFileContents(path)
 end
 
 function loadOtb(path)
-  return g_things.loadOtb(path)
+    return g_things.loadOtb(path)
 end
 
 function loadXml(path)
-  return g_things.loadXml(path)
+    return g_things.loadXml(path)
 end
 
 -- Informations
 
 function getParserType()
-  if createStats.ownParser then
-    return "Own XML parser"
-  else
-    return "OTClient inbuild XML parser"
-  end
+    if createStats.ownParser then
+        return "Own XML parser"
+    else
+        return "OTClient inbuild XML parser"
+    end
 end
